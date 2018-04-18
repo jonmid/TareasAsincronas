6947d35c44464ff0d5f8474e9c4e17f07bdd9117
JonMid <jamideros@hotmail.com>
2018-04-18 10:03:46 -0500

    Atributos de clase

--- app/src/main/java/com/example/jonmid/tareasasincronas/MainActivity.java
+++ app/src/main/java/com/example/jonmid/tareasasincronas/MainActivity.java
@@ -34,7 +34,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Atributos de clase
+    // Atributos de clase hola
     ProgressBar progressBar;
     Button button;
     TextView textView;
